0bb931f4541ae641f3a81a21c1da8eee3cf002ed
Aseda Aboagye <aseda.aboagye@gmail.com>
2015-07-15 23:23:26 -0700

    irc: Added reading from BufStream.
    Using bufstream crate from crates.io to read from the socket.
    Currently, I can read without blocking, but I need to find a way to
    flush the buffer. And then have a mechanism to finish the auth.

--- src/irc.rs
+++ src/irc.rs
@@ -1,9 +1,14 @@
+extern crate bufstream;
+
 use std::net;
 use std::io;
-use std::io::{Read,Write};
+use std::io::{Read,Write,BufRead};
+use bufstream::BufStream;
+
+const IRC_MESSAGE_MAX_LEN: usize = 510;
 
 pub struct IrcCon<'a> {
-    pub stream: net::TcpStream,
+    pub stream: bufstream::BufStream<net::TcpStream>,
     pub nick: &'a str,
 }
 
@@ -15,7 +20,7 @@ pub enum IrcMessage {
 impl <'a> IrcCon<'a> {
 	pub fn send_message(&mut self, t: IrcMessage, msg: &str) -> Result<(), io::Error> {
 		/* Format a message and send it to the server. */
-		let mut s = String::with_capacity(512);
+		let mut s = String::with_capacity(IRC_MESSAGE_MAX_LEN);
 		match t {
 			IrcMessage::User => {
 				s.push_str("USER ");
@@ -33,4 +38,53 @@ impl <'a> IrcCon<'a> {
 		/* Send it out the TcpStream. */
 		self.stream.write_all(s.as_bytes())
 	}
+
+	pub fn read_socket<'b>(&'b mut self, s: &'b mut String) -> Result<&str, io::Error> {
+		// Try and read data from the connection.
+		let num_bytes = self.stream.read_line(s).unwrap();
+		self.stream.consume(num_bytes);
+		Ok(s.as_str())
+	}
 }
+
+pub fn connect_to_server() -> Result<net::TcpStream, io::Error> {
+    // Obtain server IP address from hostname.
+    println!("Enter server to connect: ");
+    let mut hostname = String::new();
+    io::stdin().read_line(&mut hostname)
+        .ok()
+        .expect("Error reading line.");
+
+    hostname.trim();
+    let mut results = match net::lookup_host(&hostname) {
+        Ok(r) => r,
+        Err(_) => panic!("net::lookup_host failed."),
+    };
+    // Only want the first result.
+    let server = results.next();
+    let server_ip = {
+        if server.is_some() {
+            match server.unwrap() {
+                Ok(s) => s.ip(),
+                Err(_) => panic!("Missing SocketAddr! O:"),
+            }
+        } else {
+            // If net::lookup_host doesn't work then just ask for the IP.
+            println!("No results from net::lookup_host.");
+            println!("Enter IP address instead: ");
+            let mut ip_addr = String::new();
+            io::stdin().read_line(&mut ip_addr)
+                .ok()
+                .expect("Error reading line.");
+
+            match ip_addr.trim().parse() {
+                Ok(addr) => addr,
+                Err(_) => panic!("Invalid IP!"),
+            }
+        }
+    };
+    let port: u16 = 6660;
+    let conn = try!(net::TcpStream::connect((server_ip, port)));
+    Ok(conn)
+}
+
--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,36 @@
 
 // Obtain input from somewhere (maybe a text file)
 // Parse the input and perform an action based upon the input
-#![feature(ip_addr,lookup_host,convert)]
+#![feature(ip_addr,lookup_host,convert,duration,thread_sleep)]
 use std::fs::File;
 use std::io;
 use std::io::{Read,BufRead};
 use std::vec;
 use std::net;
+use std::error::Error;
+
+extern crate bufstream;
+use bufstream::BufStream;
+
+use std::thread;
+use std::time::Duration;
 
 pub mod irc;
 
 fn main() {
-    let conn = match connect_to_server() {
-        Ok(result) => result,
+    let conn = match irc::connect_to_server() {
+        Ok(result) => bufstream::BufStream::new(result),
         Err(_) => panic!("Could not connect to server."),
     };
     println!("Connected.\n===BEGIN STREAM===");
     let mut bot = irc::IrcCon { stream : conn, nick : "aaboagye_bot" };
     bot.send_message(irc::IrcMessage::User, "foo").ok();
     bot.send_message(irc::IrcMessage::Nick, "aaboagye_bot").ok();
+    let mut buffer = String::new();
+    while true {
+        println!("{}", bot.read_socket(&mut buffer).unwrap());
+        thread::sleep(Duration::new(5, 0));
+    }
 
 }
 
@@ -33,46 +45,6 @@ fn get_input(f: File) -> Vec<String> {
         input.push(l.ok().unwrap());
     }
     input
-}
-
-fn connect_to_server() -> Result<net::TcpStream, io::Error> {
-    // Obtain server IP address from hostname.
-    println!("Enter server to connect: ");
-    let mut hostname = String::new();
-    io::stdin().read_line(&mut hostname)
-        .ok()
-        .expect("Error reading line.");
-
-    hostname.trim();
-    let mut results = match net::lookup_host(&hostname) {
-        Ok(r) => r,
-        Err(_) => panic!("net::lookup_host failed."),
-    };
-    // Only want the first result.
-    let server = results.next();
-    let server_ip = {
-        if server.is_some() {
-            match server.unwrap() {
-                Ok(s) => s.ip(),
-                Err(_) => panic!("Missing SocketAddr! O:"),
-            }
-        } else {
-            // If net::lookup_host doesn't work then just ask for the IP.
-            println!("No results from net::lookup_host.");
-            println!("Enter IP address instead: ");
-            let mut ip_addr = String::new();
-            io::stdin().read_line(&mut ip_addr)
-                .ok()
-                .expect("Error reading line.");
-
-            match ip_addr.trim().parse() {
-                Ok(addr) => addr,
-                Err(_) => panic!("Invalid IP!"),
-            }
-        }
-    };
-    let port: u16 = 6660;
-    net::TcpStream::connect((server_ip, port))
 }
 
 #[cfg(test)]
