e854ecc879687d5c659a9d009d5b6e002a23d813
Aseda Aboagye <aseda.aboagye@gmail.com>
2015-07-16 20:58:55 -0700

    tests: Added read line by line test.
    Added my first unit test: reading line by line. This test tests the irc::IrcCon::read_socket() method. It's currently failing which reveals the error that I had encountered yesterday.
    
    TEST=cargo test; Verified that test_line_by_line fails.

--- src/main.rs
+++ src/main.rs
@@ -50,8 +50,46 @@ fn get_input(f: File) -> Vec<String> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs::File;
+    use std::io::Write;
+    use irc::*;
+    use bufstream::BufStream;
+    use std::str;
 
     #[test]
     fn it_works() {
+    }
+    
+    // Test to see if you can read line by line from the stream.
+    #[test]
+    fn test_line_by_line() {
+        const TEST_FILE: &'static str = "test_line_by_line.txt";
+        // Create a sample text file and populate it with test strings
+        let test_strings = vec![b"line 1\r\n", b"line 2\r\n", b"line 3\r\n"];
+        {
+            let mut f = File::create(TEST_FILE).unwrap();
+            for s in test_strings {
+                f.write(s).unwrap();
+            }
+        }
+        
+        {
+            // Open the test file
+            let f = File::open(TEST_FILE).unwrap();
+            // Create a test bot
+            let bs = BufStream::new(f);
+            let mut test_bot = irc::IrcCon { stream: bs, nick: "test_bot" };
+            let mut buffer = String::new();
+            // Verify the strings are equal
+            let test_strings = vec![b"line 1\r\n", b"line 2\r\n", b"line 3\r\n"];
+            for s in test_strings {
+                let read_str = test_bot.read_socket(&mut buffer).unwrap().as_bytes();
+                if s.len() != read_str.len() {
+                    println!("Strings are NOT the same length.");
+                    println!("Expected: `{}`\nGot: `{}`",str::from_utf8(s).unwrap(), str::from_utf8(read_str).unwrap());
+                    assert!(false);
+                }
+            }
+        }
     }
 }
